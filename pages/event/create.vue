a336c4c44742f94f509b0e61f464cac4f6d00ad0
unknown <rajavelm1999@gmail.com>
2022-02-18 00:39:38 +0530

    Testing the data

---
+++ pages/event/create.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>
+   <h1>
+    Create a Event
+   </h1>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'CretePage',
+  head() {
+    return {
+      title: 'Create Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'you can find the abour or description of the  Create event',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style >
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,62 @@
 <template>
-  <Tutorial />
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+   <h1>
+     Event
+   </h1>
+
+       <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+ </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
+import EventCard from '@/components/EventCard.vue'
+
 export default {
   name: 'IndexPage',
+  components: {
+    EventCard,
+  },
+
+  // async fetch({ store, error }) {
+  //   try {
+  //     await store.dispatch('events/fetchEvents')
+  //   } catch (e) {
+  //     error({
+  //       statusCode: 503,
+  //       message: 'Unable to fetch events at this time. Please try again.',
+  //     })
+  //   }
+  // },
+
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch('events/fetchEvents')
+      // $axios.get('http://localhost:3000/events')
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message:
+          'Unable to retrive the data. Please try again later hooouuuuu.',
+      })
+    }
+  },
+
+  head() {
+    return {
+      title: 'Event Listening ',
+    }
+  },
+  computed: mapState({
+    events: (state) => state.events.events,
+  }),
 }
 </script>
